e9171759476240cff7aec0e7486a699c2fdbbffd
borgmon <borgmon7@gmail.com>
2022-03-31 02:03:24 -0700

    update CI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: !debug ? "/next-twister/" : "",
 };
